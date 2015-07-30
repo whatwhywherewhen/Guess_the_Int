fd00ddc79ba2d379afdb9962e52a05a591efb51b
whatwhywherewhen <karl.railton@googlemail.com>
2015-07-30 01:10:59 +0100

    Final draft?

--- guess.py
+++ guess.py
@@ -1,6 +1,4 @@
-# template for "Guess the number" mini-project
-# input will come from buttons and an input field
-# all output for the game will be printed in the console
+# "Guess the number" mini-project
 
 import simplegui
 import random
@@ -9,6 +7,8 @@ import math
 num_range = 100
 tries = 0
 trylist = []
+player_score = 0
+computer_score = 0
 
 # helper function to start and restart the game
 def new_game():
@@ -17,14 +17,10 @@ def new_game():
     trylist = []
     secret_number = random.randrange(num_range)
     tries = int(math.ceil(math.log(num_range, 2))) #calculate tries based on binary search algorithm
-    print ""
-    print "====================="
-    print "New Game - Range:", num_range
-    print "Attempts remaining", tries
-    print "Guess my number"
-    print ""
-    
-    
+    print '\n', "==============================", '\n', "SCORE: Player:", player_score, \
+    "| Computer:", computer_score, '\n', "New Game - Range:", \
+    num_range, '\n', "Attempts remaining", tries, '\n',"Guess my number"
+        
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game 
@@ -37,42 +33,40 @@ def range1000():
     global num_range
     num_range = 1000
     new_game()
-
-    
+  
 def input_guess(guess):
-    # main game logic goes here	
-    global tries, trylist
-    trylist.append(guess) # Add guess to list of guesses (trylist)
-    tries = tries - 1
+    # main game logic
+    global tries, trylist, player_score, computer_score
     myguess.set_text("") # Clear the text input box
-    print "---------------------"
-    print "Guesses so far:", 
+    print "---------------------", '\n' ,"Guesses so far:", 
     for x in trylist: print x,
-    print ""
-    print "Guess was", guess
+    print "", '\n', "Guess was", guess
     if guess.isdigit() == False or int(guess) not in range(num_range):
         print "Please enter a valid number in range 0 to", num_range
-        tries = tries + 1
+    elif guess in trylist:
+        print "You already guessed that!"
     elif tries > 0 and int(guess) < secret_number:
-        print "Attempts remaining:", tries
-        print "Higher!"
+        print "Attempts remaining:", tries, '\n', "Higher!"
+        tries = tries - 1
+        trylist.append(guess) # Add guess to list of guesses (trylist)
     elif tries > 0 and int(guess) > secret_number:
-        print "Attempts remaining:", tries
-        print "Lower"
-    elif num_guess == secret_number:
+        print "Attempts remaining:", tries, '\n', "Lower"
+        tries = tries - 1
+        trylist.append(guess) # Add guess to list of guesses (trylist)
+    elif int(guess) == secret_number:
         print "Correct"
+        player_score = player_score + 1
         new_game()
     else:
-        print "Sorry no attempts left"
-        print "My Guess was", secret_number
+        print "Sorry no attempts left", '\n', "My Guess was", secret_number, '\n'
+        computer_score = computer_score + 1
         new_game()
-    
-    
+        
 # create frame
 frame = simplegui.create_frame("Guess the Interger!", 130, 130)
 
 # register event handlers for control elements and start frame
-myguess = frame.add_input("Guess", input_guess, 145)
+myguess = frame.add_input("Type your Guess!", input_guess, 145)
 button1 = frame.add_button("Range: 0 to 100", range100, 150)
 button2 = frame.add_button("Range: 0 to 1000", range1000, 150)
 
