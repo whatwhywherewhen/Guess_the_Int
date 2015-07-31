8f18b158001fce078100f9cf2062b9aec313dabc
whatwhywherewhen <karl.railton@googlemail.com>
2015-07-31 23:56:09 +0100

    Correct count behaviour. - CRITICAL
    Added scoreboard reset functionality.

--- guess.py
+++ guess.py
@@ -1,5 +1,5 @@
 # "Guess the number" mini-project
-# http://www.codeskulptor.org/#user40_J61u72yubzwXK8Z.py
+# Run via http://www.codeskulptor.org/#user40_tBR27MOHxL0bLds.py
 
 import simplegui
 import random
@@ -41,19 +41,23 @@ def input_guess(guess):
     myguess.set_text("") # Clear the text input box
     print "---------------------", '\n' ,"Guesses so far:", 
     guess = guess.strip( )
-	for x in trylist: print x,
+    for x in trylist: print x,
     print "", '\n', "Guess was", guess
-    if not guess.isdigit() or int(guess) not in range(num_range):
+    if guess == "reset":
+        player_score = 0
+        computer_score = 0
+        new_game()
+    elif not guess.isdigit() or int(guess) not in range(num_range):
         print "Please enter a valid number in range 0 to", num_range
     elif guess in trylist:
         print "You already guessed that!"
-    elif tries > 0 and int(guess) < secret_number:
+    elif tries > 1 and int(guess) < secret_number:
+        tries -= 1
         print "Attempts remaining:", tries, '\n', "Higher!"
-        tries = tries - 1
         trylist.append(guess) # Add guess to list of guesses (trylist)
-    elif tries > 0 and int(guess) > secret_number:
+    elif tries > 1 and int(guess) > secret_number:
+        tries -= 1
         print "Attempts remaining:", tries, '\n', "Lower"
-        tries = tries - 1
         trylist.append(guess) # Add guess to list of guesses (trylist)
     elif int(guess) == secret_number:
         print "Correct"
@@ -65,12 +69,13 @@ def input_guess(guess):
         new_game()
         
 # create frame
-frame = simplegui.create_frame("Guess the Interger!", 130, 130)
+frame = simplegui.create_frame("Guess the Interger!", 130, 160)
 
 # register event handlers for control elements and start frame
 myguess = frame.add_input("Type your Guess!", input_guess, 145)
 button1 = frame.add_button("Range: 0 to 100", range100, 150)
 button2 = frame.add_button("Range: 0 to 1000", range1000, 150)
+label1 = frame.add_label("Type reset to wipe scoreboard")
 
 # call new_game 
 new_game()
